ac5afe488934ba00afd66763c55bbae6edbea639
Ruben Pasillas <hobbiathan@gmail.com>
2022-05-27 14:34:57 -0600

    create model attributes

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,7 @@
-class Project < ApplicationRecord
+class Project < ApplicationRecord 
+    validates_presence_of :title,
+                          :date_year,
+                          :description,
+                          :image_url,
+                          :project_url
 end
