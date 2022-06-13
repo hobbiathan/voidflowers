9cd8354312a6d10f3ce9d033e7aa8b799692ac52
Ruben Pasillas <hobbiathan@gmail.com>
2022-06-12 19:17:15 -0600

    update user params

--- app/controllers/register_controller.rb
+++ app/controllers/register_controller.rb
@@ -29,7 +29,7 @@ class RegisterController < ApplicationController
 
   private
   def user_params
-    params.permit(:username, :password, :password_confirmation)
+    params.permit(:username, :password, :password_confirmation, :avatar)
   end
 
   def valid_password?
