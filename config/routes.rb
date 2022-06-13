25858924bb2efe851e17c9ca7c6871f271a8cf88
Ruben Pasillas <hobbiathan@gmail.com>
2022-06-12 18:50:14 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
   resources :register, only: [:index, :create]
   resources :projects, only: [:index]
+
+  # Refer to logout button to see how to work with resources instead of handrolling
+  # sessions_path(@user) for logout, something like that, I really don't wanna talk about it
+  # resources :sessions, only: [:new, :create, :destroy]
+
+  # Handrolled
+  get "/", to: "welcome#index"
+
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
 end
