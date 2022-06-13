4b2af886348ed9a23067566fc57dd946ff7bd6c9
Ruben Pasillas <hobbiathan@gmail.com>
2022-06-13 02:16:21 -0600

    magic

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,6 @@ Rails.application.configure do
   #config.active_storage.service = :local
   config.active_storage.service = :digitalocean
 
-
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
