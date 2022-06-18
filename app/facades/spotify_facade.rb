336275f0f6c789416f922dfa66f4f29373370485
Ruben Pasillas <hobbiathan@gmail.com>
2022-06-17 21:59:08 -0600

    remove unnecessary files

--- app/facades/spotify_facade.rb
+++
@@ -1,7 +0,0 @@
-require './app/poros/song'
-
-class SpotifyFacade
-  def self.get_recent
-    Song.new(SpotifyService.recently_played)
-  end
-end
--- app/services/format.rb
+++
@@ -1,5 +0,0 @@
-module Format
-  def format(response)
-    data = JSON.parse(response.body, symbolize_names: true)
-  end
-end
