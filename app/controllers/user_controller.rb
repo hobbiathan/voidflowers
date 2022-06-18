6fddbee6d4a8ffde85634becf3c8865ab81bbbec
Ruben Pasillas <hobbiathan@gmail.com>
2022-06-17 21:52:43 -0600

    create song record

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,9 +8,18 @@ class UserController < ApplicationController
   def spotify
     a = RSpotify::User.new(request.env['omniauth.auth'])
       data = RSpotify::User.new(request.env['omniauth.auth']).recently_played(limit: 1)
-      @info = { title: data[0].name, album: data[0].album.name, link: data[0].external_urls["spotify"], artist: data[0].artists[0].name }
+      song = Song.create!(title: data[0].name, album: data[0].album.name, link: data[0].external_urls["spotify"], artist: data[0].artists[0].name)
 
-      session[:has_song] = true
+      if !song.save
+        @info = { title: data[0].name }
+      else
+        @info = { title: nil }
+      end
+
       redirect_to controller: "projects", action: "index", data: @info
+  end
+
+  def song_params
+    params.permit(:title, :album, :link, :artist)
   end
 end
